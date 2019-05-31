59be710ec865b298aa29353f881a4a15f6a13698
root <root@vultr.guest>
2019-05-31 19:38:01 +0000

    clean debug

--- clean.py
+++ clean.py
@@ -1,7 +1,7 @@
 import os
   
 os.system("wc -l google.txt")
-os.system("vi google.txt -c ':g/https*:\/\/[^\/]*\/$/d'  - c':wq!'")
+os.system("vi google.txt -c ':g/https*:\/\/[^\/]*\/$/d' -c ':wq!'")
 
 #with open("banned.txt","r") as bannedFile:
 #	for site in bannedFile.readlines():
