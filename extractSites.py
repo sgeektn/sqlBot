9ebb949b6c3f2f2904ba971c96b36805176e7409
fakhfakh sami <s-man@MacBook-Pro-de-fakhfakh-2.local>
2019-06-01 22:38:03 +0200

    cancel waiting

--- extractSites.py
+++ extractSites.py
@@ -642,9 +642,7 @@ def recursiveSearch(number):
 
 
 		sitesNumber=getNumber(recursiveSitesFile, recursiveSitesFileLock)
-		readyToCheckNumber=getNumber(sitesFile, sitesFileLock)
-		if(readyToCheckNumber>5000):
-			time.sleep(60)
+		
 		#if(sitesNumber==0):
 			#recursiveBrowser.close()
 		while sitesNumber==0:
