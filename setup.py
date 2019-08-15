e9ba8b20e30481b547963fb3d2623dc04a6addd8
fakhfakh sami <s-man@MacBook-Pro-de-fakhfakh-2.local>
2019-08-15 12:51:17 +0200

    firefox install

--- setup.py
+++ setup.py
@@ -71,13 +71,15 @@ def setup():
     os.system("echo \"alias filter='zeb ; python3 extractSites.py filter'\" >> ~/.bashrc ")
     os.system("echo \"alias lss='ls -lia'\" >> ~/.bashrc ")	
     os.system("echo \"alias sites='ps aux | grep sqlmap | sed -E \'/sh -c/d\' | sed -E \'/grep/d\' '\" >> ~/.bashrc ") 
-    os.system("echo \"alias zeb='cd /root/Desktop/sqlBot'\" >> ~/.bashrc ")
+    os.system("echo \"alias zeb='cd /root/sqlBot'\" >> ~/.bashrc ")
     os.system("echo \"export PATH=$PATH:\"%s >> ~/.bashrc " %(FIREFOX_DRIVER,))
     os.system("touch " +PRIORITY_FILE +" " +ERROR_FILE +" " +SITES_FILE +" " +DORK_LIST_FILE +" " +BANNED_KEYWORDS_FILE +" " +RECURSIVE_SITES_FILE +" " +BANNING_FILE)
     if vnc:
+        os.system("apt -y install firefox")
     	os.system("apt -y install vnc4server")
     	os.system("apt -y install xfce4 xfce4-goodies")
     	os.system("apt install -y tightvncserver")
+    	os.system("apt -y install tigervnc-common")
     	os.system("apt -y install novnc websockify python-numpy")
     	os.system("openssl req -x509 -nodes -newkey rsa:2048 -keyout novnc.pem -out /etc/ssl/novnc.pem -days 365")
     	os.system("chmod 644 /etc/ssl/novnc.pem")
@@ -85,7 +87,8 @@ def setup():
     	os.system("websockify -D --web=/usr/share/novnc/ --cert=/etc/ssl/novnc.pem 6080 localhost:5901")
     	os.system("echo \"alias webvnc='websockify -D --web=/usr/share/novnc/ --cert=/etc/ssl/novnc.pem 6080 localhost:5901'\" >> ~/.bashrc ")
     	os.system("echo \"alias revnc='vncserver -kill :1 ; vncserver ; webvnc'\" >> ~/.bashrc ")
-
+    if tor:
+    	os.system("apt -y install tor")
 
 if __name__ == '__main__':
 	if clean_bot:
