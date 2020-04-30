d6c0c3aece43d726797c0a6b4fa2a77dd806186f
fakhfakh sami <s-man@MacBook-Pro-de-fakhfakh-2.local>
2020-05-01 01:56:05 +0200

    exploit todo ok

--- clean_google_txt.py
+++ clean_google_txt.py
@@ -1,5 +1,23 @@
 import os
-  
-os.system("wc -l google.txt")
-os.system("vi google.txt -c ':g/https*:\/\/[^\/]*\/$/d' -c ':wq!'")
-os.system("wc -l google.txt")
+
+
+SITES_FILE=os.getenv("SITES_FILE")
+
+def main():
+
+
+	if SITES_FILE==None:
+		print("Error : You need to set SITES_FILE\nTry : export SITES_FILE=\"sites.txt\"")
+		exit_err=True
+
+	if not os.path.isfile(SITES_FILE):
+		with open(SITES_FILE,mode="w") as new_file:
+			new_file.close() 
+
+	os.system("wc -l "+SITES_FILE)
+	os.system("vi "+SITES_FILE+" -c ':g/https*:\/\/[^\/]*\/$/d' -c ':wq!'")
+	os.system("wc -l "+SITES_FILE)
+
+
+if __name__ == '__main__':
+	main()
